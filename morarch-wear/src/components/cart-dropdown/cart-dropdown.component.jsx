bbb955941a76a2a94fc9c7a55b6ac26ecba8a4d6
Tazeem khan <tazeemkhan0712@gmail.com>
2023-02-03 16:41:43 +0530

    added in our addToCart Feature

--- morarch-wear/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ morarch-wear/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,28 @@
 import './cart-dropdown.style.scss';
 import Button from '../button/button.component';
-const CartDropDown=()=>{
- return(
-    <div className='cart-dropdown-container'>
-        <div className='cart-items'>
-            <Button>Checkout</Button>
-        </div>
-    </div>
- )
-}
+import CartItem from '../cart-item/cart-item.component';
 
-export default CartDropDown;
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
+
+
+const CartDropDown = () => {
+    const { cartItems } = useContext(CartContext);
+  
+    return (
+      <div className='cart-dropdown-container'>
+        <div className='cart-items'>
+          {cartItems.length ? (
+            cartItems.map((cartItem) => (
+              <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))
+          ) : (
+            <span className='empty-message'>Your cart is empty</span>
+          )}
+        </div>
+        <Button>GO TO CHECKOUT</Button>
+      </div>
+    );
+  };
+  
+  export default CartDropDown;
